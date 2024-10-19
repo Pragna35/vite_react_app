ebf63645e29b7055610c3840d692df2b04b51f70
pragna35 <pragnagirish11@gmail.com>
2024-10-19 12:16:27 +0530

    slight changes

--- src/props/children1.jsx
+++ src/props/children1.jsx
@@ -1,12 +1,12 @@
 
 //here we are getting  props as a component 
 const Child = (props) => {
-    console.log(props,"display component of children.prop.jsx")
+   // console.log(props,"display component of children.prop.jsx")
 
  return (
    
     <>
- <h2>i am receiving display component from children-prop.jsx </h2>
+ {/* <h2>i am receiving display component from children-prop.jsx </h2> */}
 
 {props.children}
 
